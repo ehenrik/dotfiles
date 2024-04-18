b2e34cc6122a7fc714b237bd029234ecf41b4727
Henrik Engman <henrik.engman@me.com>
2024-04-18 14:26:42 +0200

    chore: fix iterm

--- run_once_vscode.sh
+++ run_once_vscode.sh
@@ -2,5 +2,5 @@
 
 # VSCode hash: {{ include "~/.vscode/dotfiles/vscode-extensions.list" | sha256sum }}
 
-cat ~/.vscode/dotfiles/vscode-extensions.list | xargs -L 1 code --install-extension
-ln -s ~/.vscode/dotfiles/settings.json ~/Library/Application\ Support/Code/User/settings.json
+cat ~/.vscode/dotfiles/extensions.list | xargs -L 1 code --force --install-extension
+# ln -s ~/.vscode/dotfiles/settings.json ~/Library/Application\ Support/Code/User/settings.json
