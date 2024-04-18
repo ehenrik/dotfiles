76a601988eea553bc2c92fd76275ef9726c34ae4
Henrik Engman <henrik.engman@me.com>
2024-04-18 15:35:16 +0200

    use starship

--- run_once_vscode.sh
+++ run_once_vscode.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # VSCode hash: {{ include "~/.vscode/dotfiles/vscode-extensions.list" | sha256sum }}
+# Use the following command to populate the list
+code --list-extensions > /tmp/extensions.list.tmp
+diff /tmp/extensions.list.tmp ~/.vscode/dotfiles/extensions.list || cat ~/.vscode/dotfiles/extensions.list | xargs -L 1 code --force --install-extension
+rm /tmp/extensions.list.tmp
 
-cat ~/.vscode/dotfiles/extensions.list | xargs -L 1 code --force --install-extension
 # ln -s ~/.vscode/dotfiles/settings.json ~/Library/Application\ Support/Code/User/settings.json
